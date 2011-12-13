f609a681873ced13197508e76361fa0f907d44e5
Mathias Verraes <mathias@verraes.net>
2011-12-13 11:03:49 +0100

    added mapper to single field

---
+++ lib/Money/Doctrine2/MoneyType.php
@@ -0,0 +1,53 @@
+<?php
+namespace Money\Doctrine2;
+
+use Money\Money;
+use Money\Currency;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/** 
+ * Stores Money in a single field, in the smallest unit (cents). eg "EUR 100"
+ * Note that this is only usefull if you don't intend to query on this.
+ *  
+ * @example 
+ */
+class MoneyType extends Type
+{
+	const NAME = 'money';
+
+	public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+	{
+		return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+	}
+
+	public function convertToPHPValue($value, AbstractPlatform $platform)
+	{
+		if(is_null($value)) {
+			return null;
+		}
+
+		list($currency, $units) = explode(' ', $value, 2);
+		return new Money((int) $units, new Currency($currency));
+	
+	}
+
+	public function convertToDatabaseValue($value, AbstractPlatform $platform)
+	{
+		if(empty($value)) {
+			return null;
+		}
+
+		if($value instanceof Money) {
+			return (string) $value->getCurrency() . ' '. $value->getUnits();
+		}
+
+		throw ConversionException::conversionFailed($value, self::NAME);
+	}
+
+	public function getName()
+	{
+		return self::NAME;
+	}
+}
