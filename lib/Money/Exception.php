acfc85500f9f4f5eb0a88f5b328d05f61cdf4d09
Mathias Verraes <mathias@verraes.net>
2014-03-22 19:17:11 +0100

    copyrights

--- lib/Money/Exception.php
+++ lib/Money/Exception.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Money library
  *
- * Copyright (c) 2011-2013 Mathias Verraes
+ * Copyright (c) 2011-2014 Mathias Verraes
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- tests/Money/Tests/RoundingModeTest.php
+++ tests/Money/Tests/RoundingModeTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the Money library
+ *
+ * Copyright (c) 2011-2014 Mathias Verraes
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Money\Tests;
 
