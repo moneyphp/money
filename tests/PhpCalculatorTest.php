04490c14cbd46b117d0d13b59c9aae8beda9edb1
Frederik Bosch <f.bosch@genkgo.nl>
2016-01-12 17:50:50 +0100

    money's internal amount should be a string

--- tests/PhpCalculatorTest.php
+++ tests/PhpCalculatorTest.php
@@ -10,68 +10,68 @@ class PhpCalculatorTest extends \PHPUnit_Framework_TestCase
     public function testCompare()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals(1, $calculator->compare(2, 1));
-        $this->assertEquals(-1, $calculator->compare(1, 2));
-        $this->assertEquals(0, $calculator->compare(1, 1));
+        $this->assertSame(1, $calculator->compare(2, 1));
+        $this->assertSame(-1, $calculator->compare(1, 2));
+        $this->assertSame(0, $calculator->compare(1, 1));
     }
 
     public function testAdd()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals(2, $calculator->add(1, 1));
+        $this->assertSame('2', $calculator->add(1, 1));
     }
 
     public function testSubtract()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals(1, $calculator->subtract(2, 1));
+        $this->assertSame('1', $calculator->subtract(2, 1));
     }
 
     public function testMultiply()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals('1.5', $calculator->multiply(1, 1.5));
+        $this->assertSame('1.5', $calculator->multiply(1, 1.5));
     }
 
     public function testDivide()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals('1.5', $calculator->divide(3, 2));
+        $this->assertSame('1.5', $calculator->divide(3, 2));
     }
 
     public function testCeil()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals('2', $calculator->ceil(1.2));
+        $this->assertSame('2', $calculator->ceil(1.2));
     }
 
     public function testFloor()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals('2', $calculator->floor(2.7));
+        $this->assertSame('2', $calculator->floor(2.7));
     }
 
     public function testRound()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals('3', $calculator->round(2.6, Money::ROUND_HALF_EVEN));
-        $this->assertEquals('2', $calculator->round(2.5, Money::ROUND_HALF_EVEN));
-        $this->assertEquals('2', $calculator->round(2.1, Money::ROUND_HALF_ODD));
-        $this->assertEquals('3', $calculator->round(2.5, Money::ROUND_HALF_ODD));
-        $this->assertEquals('2', $calculator->round(2.5, Money::ROUND_HALF_DOWN));
-        $this->assertEquals('3', $calculator->round(2.6, Money::ROUND_HALF_DOWN));
-        $this->assertEquals('2', $calculator->round(2.2, Money::ROUND_HALF_UP));
-        $this->assertEquals('3', $calculator->round(2.5, Money::ROUND_HALF_UP));
-        $this->assertEquals('2', $calculator->round(2, Money::ROUND_HALF_UP));
-        $this->assertEquals('2', $calculator->round(2, Money::ROUND_HALF_DOWN));
-        $this->assertEquals('2', $calculator->round(2, Money::ROUND_HALF_EVEN));
-        $this->assertEquals('2', $calculator->round(2, Money::ROUND_HALF_ODD));
+        $this->assertSame('3', $calculator->round(2.6, Money::ROUND_HALF_EVEN));
+        $this->assertSame('2', $calculator->round(2.5, Money::ROUND_HALF_EVEN));
+        $this->assertSame('2', $calculator->round(2.1, Money::ROUND_HALF_ODD));
+        $this->assertSame('3', $calculator->round(2.5, Money::ROUND_HALF_ODD));
+        $this->assertSame('2', $calculator->round(2.5, Money::ROUND_HALF_DOWN));
+        $this->assertSame('3', $calculator->round(2.6, Money::ROUND_HALF_DOWN));
+        $this->assertSame('2', $calculator->round(2.2, Money::ROUND_HALF_UP));
+        $this->assertSame('3', $calculator->round(2.5, Money::ROUND_HALF_UP));
+        $this->assertSame('2', $calculator->round(2, Money::ROUND_HALF_UP));
+        $this->assertSame('2', $calculator->round(2, Money::ROUND_HALF_DOWN));
+        $this->assertSame('2', $calculator->round(2, Money::ROUND_HALF_EVEN));
+        $this->assertSame('2', $calculator->round(2, Money::ROUND_HALF_ODD));
     }
 
     public function testShare()
     {
         $calculator = new PhpCalculator();
-        $this->assertEquals('5', $calculator->share(10, 2, 4));
+        $this->assertSame('5', $calculator->share(10, 2, 4));
     }
 
     /**
