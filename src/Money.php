f430d455ec86b884bcad367209efe9ddc6a92086
Jon Gotlin <jon@jon.se>
2016-04-07 14:06:53 +0200

    More descriptive exception message

--- src/Money.php
+++ src/Money.php
@@ -251,7 +251,7 @@ final class Money implements \JsonSerializable
     private function assertOperand($operand)
     {
         if (!is_numeric($operand)) {
-            throw new \InvalidArgumentException('Operand should be a numeric value');
+            throw new \InvalidArgumentException(sprintf('Operand should be a numeric value, "%s" given.', gettype($operand)));
         }
     }
 
