192d785bc7c0b49d1ffdffc23f6c5d8c6453cb40
Teoh Han Hui <teohhanhui@gmail.com>
2016-10-07 17:27:42 +0800

    Add DecimalMoneyFormatter (#289)
    This formatter outputs a simple decimal string which is always in a consistent
    format independent of locale.

---
+++ spec/Formatter/DecimalMoneyFormatterSpec.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace spec\Money\Formatter;
+
+use Money\Currencies;
+use Money\Currency;
+use Money\Money;
+use Money\MoneyFormatter;
+use PhpSpec\ObjectBehavior;
+
+class DecimalMoneyFormatterSpec extends ObjectBehavior
+{
+    function let(Currencies $currencies)
+    {
+        $this->beConstructedWith($currencies);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Money\Formatter\DecimalMoneyFormatter');
+    }
+
+    function it_is_a_money_formatter()
+    {
+        $this->shouldImplement(MoneyFormatter::class);
+    }
+
+    function it_formats_money(Currencies $currencies)
+    {
+        $money = new Money(100, new Currency('EUR'));
+
+        $currencies->subunitFor($money->getCurrency())->willReturn(2);
+
+        $this->format($money)->shouldReturn('1.00');
+    }
+}
