b2d6ad708f8af26228c76770d3eff33b11ab7d50
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-10-20 16:55:06 +0200

    Remove exchange rate provider support

--- spec/Exchange/SwapExchangeSpec.php
+++ spec/Exchange/SwapExchangeSpec.php
@@ -2,15 +2,12 @@
 
 namespace spec\Money\Exchange;
 
-use Exchanger\Contract\ExchangeRateProvider;
-use Exchanger\Contract\ExchangeRateQuery;
 use Exchanger\Contract\ExchangeRate;
 use Exchanger\Exception\Exception;
 use Money\Currency;
 use Money\CurrencyPair;
 use Money\Exception\UnresolvableCurrencyPairException;
 use Money\Exchange\SwapExchange;
-use Prophecy\Argument;
 use Swap\Swap;
 use PhpSpec\ObjectBehavior;
 
@@ -46,37 +43,5 @@ final class SwapExchangeSpec extends ObjectBehavior
 
         $this->shouldThrow(UnresolvableCurrencyPairException::class)
             ->duringQuote(new Currency('EUR'), new Currency('XYZ'));
-    }
-
-    function it_exchanges_currencies_with_an_exchange_rate_provider(ExchangeRateProvider $exchangeRateProvider, ExchangeRate $exchangeRate)
-    {
-        $this->beConstructedWith($exchangeRateProvider);
-
-        $exchangeRateProvider->getExchangeRate(Argument::type(ExchangeRateQuery::class))->willReturn($exchangeRate);
-        $exchangeRate->getValue()->willReturn(1.0);
-
-        $currencyPair = $this->quote($base = new Currency('EUR'), $counter = new Currency('USD'));
-
-        $currencyPair->shouldHaveType(CurrencyPair::class);
-        $currencyPair->getBaseCurrency()->shouldReturn($base);
-        $currencyPair->getCounterCurrency()->shouldReturn($counter);
-        $currencyPair->getConversionRatio()->shouldReturn(1.0);
-    }
-
-    function it_throws_an_exception_when_cannot_exchange_currencies_with_an_exchange_rate_provider(ExchangeRateProvider $exchangeRateProvider)
-    {
-        $this->beConstructedWith($exchangeRateProvider);
-
-        $exchangeRateProvider->getExchangeRate(Argument::type(ExchangeRateQuery::class))->willThrow(Exception::class);
-
-        $this->shouldThrow(UnresolvableCurrencyPairException::class)
-            ->duringQuote(new Currency('EUR'), new Currency('XYZ'));
-    }
-
-    function it_throws_an_exception_when_exchange_is_not_swap_and_not_exchange_rate_provider()
-    {
-        $this->beConstructedWith(new \stdClass());
-
-        $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 }
