1f16a15e1cd046fe8f2a5af64230b7ac8e649b47
Aleksandr <aleksandr.matrosov@check24.de>
2017-09-15 14:30:42 +0200

    reformat for StyleCI

--- src/Number.php
+++ src/Number.php
@@ -254,10 +254,11 @@ final class Number
                 } else {
                     if ($moneyValue[$position - 1] === '-') {
                         $moneyValue[$position - 1] = $newValue[0];
-                        $moneyValue = '-' . $moneyValue;
+                        $moneyValue = '-'.$moneyValue;
                     } else {
                         $moneyValue[$position - 1] = $newValue[0];
                     }
+
                     break;
                 }
             }
