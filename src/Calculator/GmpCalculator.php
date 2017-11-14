4da046bb604b62de729f7cad2b737e66d1f0ff51
Frederik Bosch <f.bosch@genkgo.nl>
2017-11-14 19:47:24 +0100

    fix style within pr, ignore other

--- src/Calculator/GmpCalculator.php
+++ src/Calculator/GmpCalculator.php
@@ -91,13 +91,13 @@ final class GmpCalculator implements Calculator
                 return '0';
             }
 
-            $result = substr($resultBase, $decimalPlaces *-1);
+            $result = substr($resultBase, $decimalPlaces * -1);
             $resultLength = strlen($result);
             if ($decimalPlaces > $resultLength) {
-                return '0.' . str_pad('', $decimalPlaces - $resultLength, '0') . $result;
+                return '0.'.str_pad('', $decimalPlaces - $resultLength, '0').$result;
             }
 
-            return substr($resultBase, 0, $decimalPlaces *-1) . '.' . $result;
+            return substr($resultBase, 0, $decimalPlaces * -1).'.'.$result;
         }
 
         return gmp_strval(gmp_mul(gmp_init($amount), gmp_init((string) $multiplier)));
