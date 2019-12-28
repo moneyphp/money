350523480bb04fea59565220ee7548ced7fb054a
Mponos George <gmponos@gmail.com>
2019-12-28 12:21:38 +0200

    make IntlLocalizedDecimalParser use the DecimalParser trait

--- src/Parser/IntlLocalizedDecimalParser.php
+++ src/Parser/IntlLocalizedDecimalParser.php
@@ -5,9 +5,7 @@ namespace Money\Parser;
 use Money\Currencies;
 use Money\Currency;
 use Money\Exception\ParserException;
-use Money\Money;
 use Money\MoneyParser;
-use Money\Number;
 
 /**
  * Parses a string into a Money object using intl extension.
@@ -16,6 +14,8 @@ use Money\Number;
  */
 final class IntlLocalizedDecimalParser implements MoneyParser
 {
+    use DecimalParser;
+
     /**
      * @var \NumberFormatter
      */
@@ -70,33 +70,7 @@ final class IntlLocalizedDecimalParser implements MoneyParser
 
         $decimal = (string) $decimal;
         $subunit = $this->currencies->subunitFor($forceCurrency);
-        $decimalPosition = strpos($decimal, '.');
 
-        if (false !== $decimalPosition) {
-            $decimalLength = strlen($decimal);
-            $fractionDigits = $decimalLength - $decimalPosition - 1;
-            $decimal = str_replace('.', '', $decimal);
-            $decimal = Number::roundMoneyValue($decimal, $subunit, $fractionDigits);
-
-            if ($fractionDigits > $subunit) {
-                $decimal = substr($decimal, 0, $decimalPosition + $subunit);
-            } elseif ($fractionDigits < $subunit) {
-                $decimal .= str_pad('', $subunit - $fractionDigits, '0');
-            }
-        } else {
-            $decimal .= str_pad('', $subunit, '0');
-        }
-
-        if ('-' === $decimal[0]) {
-            $decimal = '-'.ltrim(substr($decimal, 1), '0');
-        } else {
-            $decimal = ltrim($decimal, '0');
-        }
-
-        if ('' === $decimal) {
-            $decimal = '0';
-        }
-
-        return new Money($decimal, $forceCurrency);
+        return $this->parseDecimal($decimal, $subunit, $forceCurrency);
     }
 }
