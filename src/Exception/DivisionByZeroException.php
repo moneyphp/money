3fb5ac9ac9c215b4d5648c313511b91f6006d227
Renan <renandelima@gmail.com>
2025-03-28 10:44:23 +0100

    Fix CS

--- src/Exception/DivisionByZeroException.php
+++ src/Exception/DivisionByZeroException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Money\Exception;
 
 final class DivisionByZeroException extends InvalidArgumentException
--- src/Exception/ModuloByZeroException.php
+++ src/Exception/ModuloByZeroException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Money\Exception;
 
 final class ModuloByZeroException extends InvalidArgumentException
